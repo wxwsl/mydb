a9d7002dd6bc20e52b0a586063a13e8591ec7e3f
diyshijie <90442550+diyshijie@users.noreply.github.com>
2025-03-28 13:50:47 +0800

    显示数据库内容

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 import { DurableObject } from "cloudflare:workers";
-
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 declare namespace Cloudflare {
 	interface Env {
 			MY_VARIABLE: "shrimp_value";
@@ -28,6 +15,9 @@ export default {
         const { results } = await env.DB.prepare(
             "SELECT * FROM Customers"
         ).all();
+		return new Response(JSON.stringify(results), {
+			headers: { "Content-Type": "application/json" }
+		});
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
